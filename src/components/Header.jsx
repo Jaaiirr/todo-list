7b03b770633569e9f77cb291443d531325ef02c4
Jaaiirr <jairtellezromero9678@gmail.com>
2021-11-03 21:41:48 -0600

    estilos botones header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,21 +6,20 @@ import "../styles/Header.css";
 const Header = () => {
   return (
     <>
-        <div className="tittle">
-            <header>
-                <h4 className="logo">Todo List</h4>
-            </header>
+      <div className="tittle">
+        <header>
+          <h4 className="logo">Todo List</h4>
+          {/* <h4 className="logo">Eddy Tellez</h4> */}
+        </header>
 
-            <div className="buttons btn-bg bg-2 ">
-                <div className="btn btn-2">
-                    <button > All Task</button>
-                    <button>Completed Task</button>
-                    <button>Uncompleted Task</button>
-                </div>
-            </div>
-            
+        <div className="buttons btn-bg bg-2 ">
+          <div className="btn btn-2 flex buttons">
+            <button> All Task</button>
+            <button>Completed Task</button>
+            <button>Uncompleted Task</button>
+          </div>
         </div>
-        
+      </div>
     </>
   );
 };
